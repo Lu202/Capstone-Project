262a6d49ba36a74f97d97955d316e0febad3544e
Luljana Selmanoski <lselmanoski@gmx.de>
2022-05-21 22:18:48 +0200

    Update style

--- src/components/card-open/Card.styled.js
+++ src/components/card-open/Card.styled.js
@@ -5,7 +5,7 @@ const CardContainer = styled.div`
 	height: 550px;
 	border-radius: 30px;
 	font-family: Arial, Helvetica, sans-serif;
-	font-size: 0.8rem;
+	font-size: 0.7rem;
 	padding: 0.8rem;
 	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 	background: #f0f0f0;

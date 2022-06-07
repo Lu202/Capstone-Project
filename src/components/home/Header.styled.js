8a27c462fe9b70f194bffb4a69d0226fbb7c24b5
Luljana Selmanoski <lselmanoski@gmx.de>
2022-06-07 10:47:41 +0200

    Add landing-page

---
+++ src/components/home/Header.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Header = styled.h4`
+	 {
+        display: inline-block
+        font-family: Helvetica Neue;
+	    padding-right:7.5rem;
+        padding-top:2.5rem;
+        padding-bottom:2.5rem;
+        color: 000000;
+        line-height: 1.3rem;
+        letter-spacing: 0.1em;
+		font-size: 1.1em;
+		color: #000000;
+		text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      
+	
+	}
+`;
+
+export default Header;
---
+++ src/components/home/WrapperPage.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const WrapperPage = styled.div`
+	 {
+		witdh: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+`;
+
+export default WrapperPage;
